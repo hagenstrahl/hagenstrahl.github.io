fe7ee0f2da6b631e5647b338084cf283bf5e5379
Hagen Strahl <hagen.strahl@googlemail.com>
2020-01-21 16:35:53 +0100

    upload property-configurator as demo

---
+++ property-configurator/precache-manifest.8a522686a91ea95bad593a5fba6a18cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1e977b93845a57ebddc694087c788f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "540924a97619832d70bc",
+    "url": "/static/css/main.19fe8843.chunk.css"
+  },
+  {
+    "revision": "53ba5d8822cd319ed7e6",
+    "url": "/static/js/2.9aa65e17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9aa65e17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "540924a97619832d70bc",
+    "url": "/static/js/main.31b65693.chunk.js"
+  },
+  {
+    "revision": "6445af5a8d20dbfba03f",
+    "url": "/static/js/runtime-main.f6e4b71b.js"
+  },
+  {
+    "revision": "c31489a16d5c94b8a62e74cd30d04177",
+    "url": "/static/media/configurator-picture.c31489a1.jpg"
+  },
+  {
+    "revision": "0f32251298ed729359281f530d23362f",
+    "url": "/static/media/download-appstore.0f322512.png"
+  },
+  {
+    "revision": "98d872cc1a3a415da39411d9a602cc20",
+    "url": "/static/media/download-playstore.98d872cc.png"
+  },
+  {
+    "revision": "7d693f8b8e53d13aeba52b8f0981e62e",
+    "url": "/static/media/model-luxus.7d693f8b.jpg"
+  }
+]);
